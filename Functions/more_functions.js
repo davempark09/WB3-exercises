56db1a3e25fe253396a115f83f6baa1c4d4365f2
davempark09 <43024345+davempark09@users.noreply.github.com>
2023-05-02 16:02:11 -0400

    Current progress

---
+++ Functions/more_functions.js
@@ -0,0 +1,6 @@
+function displayMailingLabel (name, address, city, state, zip) {
+    console.log(name);
+    console.log(address);
+    console.log(city + " " + state + " " + zip);
+}
+displayMailingLabel("david", "1285 mauldin road", "calhoun", "GA", 30701);
---
+++ Functions/simple_functions.js
@@ -0,0 +1,15 @@
+function favoriteThings () {
+    console.log("David");
+    console.log("Good Will Hunting");
+    console.log("Linkin Park");
+}
+function whyImHere() {
+    console.log("I joined this program to learn new things and get a career I'd love.");
+}
+function favoritePlace () {
+    console.log("My favorite place is home because it's where I feel most at peace.")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
